eb0bddeab416d9ea6e403fa12af492c071cd3c58
Felipe Vargas <felipelwvargas@gmail.com>
2025-11-14 10:57:05 -0300

    separando salvar na API em um metodo separado

--- lib/services/transaction_service.dart
+++ lib/services/transaction_service.dart
@@ -1,6 +1,7 @@
 import 'package:amigo_secreto/helpers/helper_taxes.dart';
 import 'package:amigo_secreto/models/account.dart';
 import 'package:amigo_secreto/services/account_services.dart';
+import 'package:amigo_secreto/models/account.dart';
 
 class TransactionService{
   AccountServices _accountServices = AccountServices();
@@ -8,9 +9,9 @@ class TransactionService{
     List<Account> listAccounts = await _accountServices.getAll();
     Account senderAccount = listAccounts.firstWhere((acc) => acc.id == idSender,) ;
     Account receiverAccount = listAccounts.firstWhere((acc) => acc.id == idReceiver,);
-    print(senderAccount);
-    print(receiverAccount);
-    print(calculateTaxesByAccount(sender: senderAccount, amount: amount));
-    }
+    double taxes = calculateTaxesByAccount(sender: senderAccount, amount: amount);
 
+    
+
+    }
 }
